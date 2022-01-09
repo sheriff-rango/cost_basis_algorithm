6c71e7fb00fa644a27b10d721dff13d1f038f937
sheriff0613 <95250818+sheriff0613@users.noreply.github.com>
2022-01-10 03:22:34 +0800

    modified server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ let history = null;
 
 const app = express();
 app.use(cors());
-const PORT = 3200;
+// const PORT = 3200;
+const PORT = process.env?.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at (http://localhost:${PORT}`);
 });
